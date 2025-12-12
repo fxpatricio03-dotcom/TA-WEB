72ce5c21e2c926a8d7144fa49c4a5619f6c43208
fxpatricio03-dotcom <fxpatricio03@gmail.com>
2025-12-12 18:17:15 +0700

    Reset DB_PASS to an empty string

--- config.php
+++ config.php
@@ -7,7 +7,7 @@ if (!defined('DB_USER')) {
     define('DB_USER', 'root');
 }
 if (!defined('DB_PASS')) {
-    define('DB_PASS', 'ppkpi');
+    define('DB_PASS', '');
 }
 if (!defined('DB_NAME')) {
     define('DB_NAME', 'kios_kane');
